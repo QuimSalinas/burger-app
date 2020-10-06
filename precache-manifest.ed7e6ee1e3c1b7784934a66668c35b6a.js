4f4fce827614cb140448971ffef8998070523ab1
QuimSalinas <salinas.volta2@gmail.com>
2020-10-06 16:48:35 +0200

    Updates

---
+++ precache-manifest.ed7e6ee1e3c1b7784934a66668c35b6a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53bd9f376ba21efddff5d76a272b1d8",
+    "url": "/QuimSalinas/burger-app/index.html"
+  },
+  {
+    "revision": "1066cf4ace09f155c07f",
+    "url": "/QuimSalinas/burger-app/static/css/3.627d732b.chunk.css"
+  },
+  {
+    "revision": "26207a0cb0a34a35e7a6",
+    "url": "/QuimSalinas/burger-app/static/css/4.98737583.chunk.css"
+  },
+  {
+    "revision": "d76d38211e3fe786475b",
+    "url": "/QuimSalinas/burger-app/static/css/5.29466d00.chunk.css"
+  },
+  {
+    "revision": "d69dcb573b6d170a2b61",
+    "url": "/QuimSalinas/burger-app/static/css/main.dbb8b59a.chunk.css"
+  },
+  {
+    "revision": "1b08b4bb3619aae4c5d1",
+    "url": "/QuimSalinas/burger-app/static/js/2.773fc4f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/QuimSalinas/burger-app/static/js/2.773fc4f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1066cf4ace09f155c07f",
+    "url": "/QuimSalinas/burger-app/static/js/3.b90c33e0.chunk.js"
+  },
+  {
+    "revision": "26207a0cb0a34a35e7a6",
+    "url": "/QuimSalinas/burger-app/static/js/4.432a7352.chunk.js"
+  },
+  {
+    "revision": "d76d38211e3fe786475b",
+    "url": "/QuimSalinas/burger-app/static/js/5.46eb24e1.chunk.js"
+  },
+  {
+    "revision": "d69dcb573b6d170a2b61",
+    "url": "/QuimSalinas/burger-app/static/js/main.4ab218b3.chunk.js"
+  },
+  {
+    "revision": "35b8e64522ee5e46983d",
+    "url": "/QuimSalinas/burger-app/static/js/runtime-main.aa57f2d9.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/QuimSalinas/burger-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
